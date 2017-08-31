02a0c10fbf601596bd7f1eab03dd5573b0fa7b31
kentaro.maeda <many.front@gmail.com>
2017-08-31 10:14:29 +0900

    fix type alias from DBIOAction to DBIO

--- src/main/scala/repository/SlickContext.scala
+++ src/main/scala/repository/SlickContext.scala
@@ -1,6 +1,5 @@
 package repository
 
-import model.{Tables}
 import slick.jdbc.JdbcProfile
 
 import scala.concurrent.{ExecutionContext, Future}
@@ -11,17 +10,17 @@ trait SlickContext{
 
   import profile.api._
 
-  type Action[R] = DBIOAction[R, NoStream, Effect.All]
+  type Action[R] = DBIO[R]
   type Storage = Database
   def success[R](r:R):Action[R] = DBIO.successful(r)
   def fail(r:Exception):Action[Exception] = DBIO.failed(r)
   def withTransaction[R](db:Storage)(action:Action[R]):Future[R] = db.run(action.transactionally)
 
   implicit def DBIOMonadInstance(implicit ec:ExecutionContext) = new Monad[Action] {
-    override def map[A, B](fa: DBIOAction[A, NoStream, Effect.All])(f: (A) => B) = fa.map(f)
+    override def map[A, B](fa:Action[A])(f: (A) => B) = fa.map(f)
 
-    override def flatMap[A, B](fa: DBIOAction[A, NoStream, Effect.All])(f: (A) => DBIOAction[B, NoStream, Effect.All]) = fa.flatMap(f)
+    override def flatMap[A, B](fa:Action[A])(f: (A) => Action[B]) = fa.flatMap(f)
 
-    override def filter[A](fa: DBIOAction[A, NoStream, Effect.All])(f: (A) => Boolean) = fa.filter(f)
+    override def filter[A](fa:Action[A])(f: (A) => Boolean) = fa.filter(f)
   }
 }
